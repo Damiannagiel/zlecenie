7c5f69f0e613a560f0fe2b706bac3e39f28a469c
Damiannagiel <internetowa.gielda.uslug@gmail.com>
2018-01-06 23:01:06 +0100

    0.2.3.1
    Dodano moduł dodawania/edytowania avatara przez użytkownika

--- panel_uzytkownika/edycja/avatar_skrypt.php
+++ panel_uzytkownika/edycja/avatar_skrypt.php
@@ -15,11 +15,44 @@
         $_SESSION['avatar_size']="Wbyrałeś zbyt duży plik";
     }
     else{
-        
+        //sprawdzam czy nie wystąpił błąd podczas wysyłania pliku
+        if(!sprawdz_wyslanie_pliku($plik)){
+            $plik_ok=false;
+            $_SESSION['avatar_wysylanie']="Wystąpił błąd podczas wysyłania pliku. Przepraszamy za utrudnienia i prosimy spróbować ponownie później.";
+        }
+        else{
+            //sprawdzam czy plik ma prawidłowy typ MIME?
+            if (!sprawdz_typ_jpg($plik)){
+                if(!sprawdz_typ_png($plik)){
+                    $plik_ok=false;
+                    $_SESSION['avatar_mime']="przesłany plik nie jest prawidłowym plikiem .jpg lub .png!";
+                }
+                else $rozszerzenie='.png';
+            }
+            else $rozszerzenie='.jpg';
+        }
     }
     
+    //avarar przeszedł walidację
     if($plik_ok){
         echo "super";
+        //określam lokalizację do zapisania
+        $lokalizacja = '../../public_profile/avatar/'.$id.$rozszerzenie;
+            //sprawdzam czy plik został przesłany przez formulaż HTTP
+            if (is_uploaded_file($plik['tmp_name'])){
+                //sprawdzam czy user posiada już avatar .jpg lub .png i ewentualnie go usuwam
+                if(file_exists('../../public_profile/avatar/'.$id.'.jpg'))unlink('../../public_profile/avatar/'.$id.'.jpg');
+                if(file_exists('../../public_profile/avatar/'.$id.'.png'))unlink('../../public_profile/avatar/'.$id.'.png');
+                    //zapisuję nowy avatar
+                    if (!move_uploaded_file($plik['tmp_name'], $lokalizacja)){
+                        echo "przykro nam coś poszło nie tak... prosimy spróbować ponownie za chwilę.";
+                    }
+                    else header('Location:../user.php');
+          }
+          else
+          {
+                echo "przykro nam coś poszło nie tak... prosimy spróbować ponownie za chwilę.";
+          }
     }
     else{
         echo "zdjęcie nie przeszło walidacji";
