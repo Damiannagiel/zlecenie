d404b82c96404e56e7fd9569d5e44a5d1275ae31
Damiannagiel <internetowa.gielda.uslug@gmail.com>
2018-01-16 23:05:44 +0100

    Revert "Revert "0.2.9""
    This reverts commit cc50bc24f14c514ce074119cd30df520c2b17292.

--- kategoria/mapa/ogloszenie.php
+++ kategoria/mapa/ogloszenie.php
@@ -1,9 +1,16 @@
-<a class="ad" href="../ogloszenie/<?php echo $link[$i]?>.php">
+<a class="ad" href="<?php echo $link[$i]?>">
 	<article class="ad-mini">
 		<div class="img">			
 			<?php
-			if($pobierz[$i]['ile_zdjec']==0) echo '<img src="../ogloszenie/img/brak_zdjecia.jpg"/>';
-			else echo '<img src="../ogloszenie/img/'.$pobierz[$i]["id"].'/1.jpg"/>';
+                        if(isset($overriding)){
+                            //zmienna z archiwum/index.php nakazująca dodać do zdjęcia folder nadrzędny
+                            if($pobierz[$i]['ile_zdjec']==0) echo '<img src="../../ogloszenie/img/brak_zdjecia.jpg"/>';
+                            else echo '<img src="../../ogloszenie/img/'.$pobierz[$i]["id"].'/1.jpg"/>';
+                        }
+                        else{
+                            if($pobierz[$i]['ile_zdjec']==0) echo '<img src="../ogloszenie/img/brak_zdjecia.jpg"/>';
+                            else echo '<img src="../ogloszenie/img/'.$pobierz[$i]["id"].'/1.jpg"/>';
+                        }
 			?>
 		</div>		
 			<div class="info">
---
+++ ogloszenie/archiwum/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<?php
+		include_once '../../szablon/nav_head2.php';
+		$DOCUMENT_ROOT=$_SERVER['DOCUMENT_ROOT'];
+                
+                //łączę się z bazą
+		require_once ($DOCUMENT_ROOT.'/../ini/FunkcjePHP/polacz_z_baza.php');
+		include($DOCUMENT_ROOT.'/../ini/funkcjePHP/connect.php');
+                include($DOCUMENT_ROOT.'/../ini/FunkcjePHP/formatuj_ciag.php');
+                include ($DOCUMENT_ROOT.'/../ini/FunkcjePHP/funkcje_data_time.php');
+		if(isset($polaczenie)){
+                    $current_date=date('Y-m-d H:i:s');
+                    $subtract_2_day=odejmij_dni(90);
+                    $subtract_90_day=odejmij_dni(182);
+
+                    //sprawdź ile jest łącznie zarchiwizowanych ogłoszeń (ogłoszenia arhiwizuje się 3 miesiące)
+                    $zapytanie='SELECT id FROM ogloszenia WHERE (koniec < "'.$current_date.'") AND (koniec > "'.$subtract_90_day.'")';
+                    $all=pobierz_zapytanie($polaczenie,$zapytanie);
+                    $how_much=sizeof($all);
+                    $ile_pasuje='('.$how_much.' zarchiwizowanych ogłoszeń łącznie)';
+                        
+                    //pobierz ogłoszenia zarchiwizowane w ciągu ostatnich 2 dni
+                    $zapytanie='SELECT * FROM ogloszenia WHERE (koniec < "'.$current_date.'") AND (koniec > "'.$subtract_2_day.'")';
+                    $pobierz=pobierz_zapytanie($polaczenie,$zapytanie);
+                    if($pobierz){
+                        $ile=sizeof($pobierz);
+                        $overriding=true;//zmienna do sprawdzenia w ogloszenie.php czy dodać folder nadrzędny do zdjęć
+                        for($i=0;$i<$ile;$i++)
+                        {
+                            $link_beta=usun_ogonki($pobierz[$i]['tytul']).'_'.$pobierz[$i]['id'];
+                            $link[$i]='../../ogloszenie/'.$link_beta.'.php';
+                            $zasieg[$i]=powiaty_ogloszenie($pobierz[$i]['zasieg'],$pobierz[$i]['miasto'],2,2);
+                            if($pobierz[$i]['cena']!="")
+                            {
+                                    $cena[$i]=$pobierz[$i]['cena'].' zł '.$pobierz[$i]['cena_za'];
+                            }
+                            else
+                            {
+                                    $cena[$i]=$pobierz[$i]['cena_za'];
+                            }
+                        }
+                    }
+                    $polaczenie->close();
+		}
+		else exit;
+	?>
+	
+	<title>igu.com.pl - Archiwum ogłoszeń</title>
+	
+	<meta name="description" content="Opis w Google" />
+	<link href="../../kategoria/css/mini.css" type="text/css" rel="stylesheet"/>
+	<link href="../../ogloszenie_wyswietl/mini.css" type="text/css" rel="stylesheet"/>
+        <link href="archiwum.css" type="text/css" rel="stylesheet"/>
+</head>
+
+<?php
+	include_once '../../szablon/nav_body2.php';
+	include_once '../../szablon/nav_category2.php';
+?>
+        <article class="page_content">
+            <header class="page_header">
+                    <h2>Ostatnio zarchiwizowane ogłoszenia</h2>
+                    <p class="nawias"><?php echo $ile_pasuje;?></p>
+            </header>
+
+            <?php
+                                    if($pobierz)
+                                    {
+                                            for($i=0;$i<$ile;$i++)
+                                            {
+                                                    include '../../kategoria/mapa/ogloszenie.php';
+                                            }
+                                    }
+                            ?>
+        </article>
+<?php
+	include_once '../../szablon/stopka2.php';
+?>
