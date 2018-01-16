1f29c5f05a3b9b08c7ea475a31d75076da7e779e
Damiannagiel <internetowa.gielda.uslug@gmail.com>
2018-01-16 23:02:38 +0100

    Revert "Revert "Revert "0.2.10.1"""
    This reverts commit 59f3e9b6c3b496ac4b845b9aadb8efc848aaa5c4.

--- panel_uzytkownika/ogloszenia/wyswietl_ogloszenie.php
+++ panel_uzytkownika/ogloszenia/wyswietl_ogloszenie.php
@@ -1,21 +1,17 @@
-<a href="../ogloszenie/<?php echo $link[$i]; ?>.php"><div class="wyswietl_ogloszenie <?php if($active[$i]!=true)echo"archives"; ?>">
+<div id="ogl"><a href="../ogloszenie/<?php echo $link[$i]; ?>.php"><div class="wyswietl_ogloszenie">
 		<div class="box info_ogloszenie">
 			<div class="tytul_ogloszenie"><?php echo $ogloszenia[$i]['tytul']; ?></div>
 			<div class="koniec_ogloszenie"><span class="info">Wygasa:</span> <span class="value"><?php echo $ogloszenia[$i]['koniec']; ?></span></div>
 			<div class="kategoria_ogloszenie"><span class="info">Kategoria:</span> <span class="value"><?php echo $ogloszenia[$i]['kategoria']; ?></span></div>
 			<div class="zasieg_ogloszenie"><span class="info">Zasięg:</span> <span class="value"><?php echo $zasieg[$i]; ?></span></div>
 		</div>
-		<div class="button-div"><?php
-                    if($active[$i]==true){
-			echo '<a href="../ogloszenie_add/edytuj_ogloszenie.php?ogloszenie='.$ogloszenia[$i]['id'].'"><button class="button-green">Edytuj</button></a>';
-			echo '<a><button class="button-red" id="del_'.$ogloszenia[$i]['id'].'">Usuń</button></a>';
-                    }
-                    else{
-                        echo '<a href="../ogloszenie_add/edytuj_ogloszenie.php?ogloszenie='.$ogloszenia[$i]['id'].'&resume=1"><button class="button-green">Wznów</button></a>';
-                    }
-		?></div>
+		<div class="button-div">
+			<a href="../ogloszenie_add/edytuj_ogloszenie.php?ogloszenie=<?php echo $ogloszenia[$i]['id'] ?>"><button class="button-green">Edytuj</button></a>
+			<a><button class="button-red" id="del_<?php echo $ogloszenia[$i]['id'];?>">Usuń</button></a>
+		</div>
 		<div style="clear:both"></div>
 </div></a>
+</div>
 
 <script>
 var click = "#del_"+<?php echo $ogloszenia[$i]['id'];?>;
