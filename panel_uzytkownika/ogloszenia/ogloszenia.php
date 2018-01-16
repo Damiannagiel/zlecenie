2409c4a1e3660c058cebef627db0a58da1d0e82e
Damiannagiel <internetowa.gielda.uslug@gmail.com>
2018-01-16 23:05:27 +0100

    Revert "Revert "Revert "Revert "0.2.10.1""""
    This reverts commit 1f29c5f05a3b9b08c7ea475a31d75076da7e779e.

--- panel_uzytkownika/ogloszenia/ogloszenia.php
+++ panel_uzytkownika/ogloszenia/ogloszenia.php
@@ -1,22 +1,39 @@
 <?php 
-	$DOCUMENT_ROOT=$_SERVER['DOCUMENT_ROOT'];
-	require_once ($DOCUMENT_ROOT.'/../ini/skryptyPHP/ogloszenia_skrypt.php');
+    $DOCUMENT_ROOT=$_SERVER['DOCUMENT_ROOT'];
+    require_once ($DOCUMENT_ROOT.'/../ini/skryptyPHP/ogloszenia_skrypt.php');
 ?>
 
 <h3>Moje aktywne ogłoszenia</h3>
 <?php
-	if($ogloszenia)
-	{
-		for($i=0;$i<$ile_ogloszen;$i++)
-		{
-			if($ogloszenia[$i])
-			{
-				require('wyswietl_ogloszenie.php');
-			}
-		}
-	}
-	else echo "<p>-brak aktywnych ogłoszeń</p>";
+    if($ogloszenia)
+    {
+        for($i=0;$i<$ile_ogloszen;$i++)
+        {
+            if($ogloszenia[$i])
+            {
+                    require('wyswietl_ogloszenie.php');
+            }
+        }
+    }
+    else echo "<p>-brak aktwnych ogłoszeń</p>";
 ?>
 <h3 class="no_margin_bottom">Moje nieaktywne ogłoszenia</h3>
 <p class="nawias">(Nieaktywne ogłoszenia pozostają w archiwum przez 3 miesiące następnie są usuwane)</p>
-<p>-brak zarchiwizowanych ogłoszeń</p>
+<?php
+    if($archives)
+    {
+        $ogloszenia=$archives;
+        $ile_ogloszen=$ile_archives;
+        $link=$link_archives;
+        $zasieg=$zasieg_archives;
+        $active=$no_active;
+        for($i=0;$i<$ile_ogloszen;$i++)
+        {
+            if($ogloszenia[$i])
+            {
+                    require('wyswietl_ogloszenie.php');
+            }
+        }
+    }
+    else echo "<p>-brak zarchiwizowanych ogłoszeń</p>";
+?>
