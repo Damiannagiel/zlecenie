7edd55c2cdb86f74809ea233b93e64e930853dde
Damiannagiel <internetowa.gielda.uslug@gmail.com>
2018-02-27 21:45:36 +0100

    0.3.3.7

--- logowanie/reset_pass_script.php
+++ logowanie/reset_pass_script.php
@@ -8,15 +8,15 @@ require ($DOCUMENT_ROOT.'/../ini/klasyPHP/class/MyError.php');
 require ($DOCUMENT_ROOT.'/../ini/klasyPHP/class/ResetPass.php');
 require ($DOCUMENT_ROOT.'/../ini/klasyPHP/class/Login.php');
 require ($DOCUMENT_ROOT.'/../ini/klasyPHP/class/Email.php');
+
 session_start();
 
-    $input = Input::CreateInput($_POST['identity'],"identity");
-    $validError = new ValidError();
-    $validInput = ValidInput::createValidInput($input,$validError);
-    $validInput->valid();
+    $input = Input::CreateInput($_POST['identity'],"identity");// obiekt z danymi z input
+    $validError = new ValidError();// obiekt obsługi błędów
+    $validInput = ValidInput::createValidInput($input,$validError);// obiekt przeprowadzający walidację
+    $validInput->valid();// wykonaj walidację
     if($validError->checkErrors()){
-        // dalsza część skryptu
-        echo "walidacja przebiegła pomyslnie";
+        require_once ($DOCUMENT_ROOT.'/../ini/FunkcjePHP/polacz_z_baza.php');// nawiąż połączenie z bazą danych
     }
     else{
         $_SESSION['validError'] = $validError;
