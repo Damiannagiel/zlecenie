3ee89547e114bff4ffeede6f1ff00746dfde1b1a
Damiannagiel <internetowa.gielda.uslug@gmail.com>
2018-03-02 22:10:10 +0100

    0.3.9.1
    dodano sprawdzanie użytkownika w bazie danych

--- logowanie/reset_pass_script.php
+++ logowanie/reset_pass_script.php
@@ -6,8 +6,8 @@ require ($DOCUMENT_ROOT.'/../ini/klasyPHP/class/Input.php');
 require ($DOCUMENT_ROOT.'/../ini/klasyPHP/class/ValidInput.php');
 require ($DOCUMENT_ROOT.'/../ini/klasyPHP/class/MyError.php');
 require ($DOCUMENT_ROOT.'/../ini/klasyPHP/class/ResetPass.php');
-require ($DOCUMENT_ROOT.'/../ini/klasyPHP/class/Login.php');
-require ($DOCUMENT_ROOT.'/../ini/klasyPHP/class/Email.php');
+require ($DOCUMENT_ROOT.'/../ini/klasyPHP/class/User.php');
+require ($DOCUMENT_ROOT.'/../ini/klasyPHP/class/DBConnect.php');
 
 session_start();
 
@@ -17,6 +17,14 @@ session_start();
     $validInput->valid();// wykonaj walidację
     if($validError->checkErrors()){
         require_once ($DOCUMENT_ROOT.'/../ini/FunkcjePHP/polacz_z_baza.php');// nawiąż połączenie z bazą danych
+        if($connection){
+            $dbError = new DataBaseError();
+            $dbSelect = new DataBaseSelect($connection,$dbError);
+            $requirement = $input->getName().' = "'.$input->getValue().'"';
+            $user = $dbSelect->UserDownload("id,login,email,deleted",$requirement);
+            
+            echo $user->lately;
+        }
     }
     else{
         $_SESSION['validError'] = $validError;
