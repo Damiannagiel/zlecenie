a375ebd5887d04687900cc85b7166ef1d55e2b27
Damiannagiel <internetowa.gielda.uslug@gmail.com>
2018-03-06 22:07:34 +0100

    0.3.10
    skrypt przeprowadza walidację danych (włącznie z recaptchą) zapisuje kod resetujący w bazie danych i wysyła email z kodem resetującym

--- logowanie/reset_pass_script.php
+++ logowanie/reset_pass_script.php
@@ -8,7 +8,7 @@ require ($DOCUMENT_ROOT.'/../ini/class/classMyError.php');
 require ($DOCUMENT_ROOT.'/../ini/class/classResetPass.php');
 require ($DOCUMENT_ROOT.'/../ini/class/classUser.php');
 require ($DOCUMENT_ROOT.'/../ini/class/classDBConnect.php');
-require ($DOCUMENT_ROOT.'/../ini/class/classSednEmail.php');
+require ($DOCUMENT_ROOT.'/../ini/class/classSendEmail.php');
 
 session_start();
 
@@ -17,6 +17,7 @@ try{
     $validError = new ValidError();// obiekt obsługi błędów
     $validInput = ValidInput::createValidInput($input,$validError);// obiekt przeprowadzający walidację
     $validInput->valid();// wykonaj walidację
+    ValidInput::validRecaptcha($_POST['g-recaptcha-response'],$validError);
     if($validError->checkErrors()){
         //wykonaj jeżeli nie wykryto błędów walidacji
         require_once ($DOCUMENT_ROOT.'/../ini/FunkcjePHP/polacz_z_baza.php');// nawiąż połączenie z bazą danych
@@ -27,14 +28,19 @@ try{
             $user = NewUser::createUser($dbSelect,"id,login,email",$requirement);// pobierz dane użytkownika
             if($dbError->checkErrors()){
                 // wykonaj jeżeli udało się pobrać użytkownika
-                $microtime = microtime(true);
-                $micro = explode('.',$microtime);
-                $code = rand(100000,429495).$micro[1];
-                $date = date('Y-m-d');
+                $date = date('Y-m-d');// ustal datę 
                 $keys = ["resetPass","resetPassTime"];
                 $value = [$code,$date]; 
                 $dbEdit = new DataBaseEdit($connection,$dbError);
                 $dbEdit->editUser($keys,$value,$user->getId());
+                
+                $sendEmail = new SendResetPassEmail;
+                $sendEmail->addAddress($user->getEmail(),$user->getLogin());
+                $sendEmail->setContents($user,$code);
+                if($sendEmail->sendEmail()){
+                    echo "wszystko ok";
+                }
+                else echo "dupa";
                 
             }
             else{
