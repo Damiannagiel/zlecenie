ba0615b6dd7852f822141068f91b8f33b7bd99fb
Damiannagiel <internetowa.gielda.uslug@gmail.com>
2018-04-08 19:42:39 +0200

    0.7/1
    -rozpoczęto prace nad regulaminem
    -dodano definicje
    -dodano potanowienia wstępne

--- regulamin/index.php
+++ regulamin/index.php
@@ -20,24 +20,66 @@
 	?>
     <article>
 	<div id="regulations">
-	<h2>Regulamin korzystania z serwisu Internetowa Giełda Usług</h2>
-	<h6>Postanowienia wstępne</h6>
-	<ul>
-		<li><p>
-		Niniejszy regulamin sporządzony został zgodnie z prawem obowiązującym na terytorium Rzeczpospolitej Polskiej. Zawiera wszelkie zasady funkcionowania i korzystania z serwisu Internetowa Giełda Usług znajdującego się pod adresem <a href="../index.php">www.igu.com.pl</a>
-	</p></li>
-	</ul>
-	<h6>Definicje</h6>
-		<ol>
-			<li class="def"><span>Regulamin</span><p>niniejszy dokument</p></li>
-			<li class="def"><span>IGU</span><p>serwis internetowy Internetowa Giełda Usług</p></li>
-		</ol>
-	<div style="clear:both;"></div>
-	<h6>Artykuł 1 Ogólne warunki korzystania z Serwisu</h6>
-		<ol>
-			<li class="nr"><span>1.1</span><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean commodo, lectus id volutpat imperdiet, enim quam sagittis lorem, sed vehicula dolor ligula vel lectus. Mauris.</p></li>
-			<li class="nr"><span>1.2</span><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed sodales augue. Maecenas sodales mauris eu mi egestas feugiat. Vestibulum vitae leo vitae tortor interdum.</p></li>
-		</ol>
+            <h4>Regulamin korzystania z serwisu Internetowa Giełda Usług</h4>
+            <h6>Postanowienia wstępne</h6>
+            <ul>
+                <li><p>
+                    Niniejszy regulamin sporządzony został zgodnie z prawem obowiązującym na terytorium Rzeczpospolitej Polskiej. Zawiera wszelkie zasady funkcionowania i korzystania z serwisu Internetowa Giełda Usług znajdującego się pod adresem <a href="../index.php">www.igu.com.pl</a>
+                </p></li>
+            </ul>
+            <h6>I Definicje</h6>
+            <ol>
+                <li class="def"><span>IGU lub serwis</span><p>internetowy serwis ogłoszeniowy Internetowa Giełda Usług dostępny w domenie internetowej internetowagieldauslug.pl prowadzony przez Administratora.</p></li>
+                <li class="def"><span>Użytkownik</span><p>osoba fizyczna, która ukończyła 18 lat i posiada pełną zdolność do czynności prawnych, osoba prawna lub jednostka organizacyjna nieposiadająca osobowości prawnej, ale mogąca we własnym imieniu nabywać prawa oraz zaciągać zobowiązania, która w trybie przewidzianym w regulaminie korzysta z serwisu.</p></li>
+                <li class="def"><span>Zarejestrowany Użytkownik</span><p>Użytkownik, który dokonał rejestracji i aktywacji konta.</p></li>
+                <li class="def"><span>Ogłoszenie</span><p>strona/przestrzeń serwerowa IGU, udostępniona Użytkownikowi, zawierająca stworzone przez niego ogłoszenie dotyczące chęci wykonania lub zlecenia wykonania usługi.</p></li>
+                <li class="def"><span>Konto</span><p>przydzielone danemu Użytkownikowi, identyfikowane za pomocą loginu i hasła, miejsce w serwisie, za pomocą którego wprowadza on dane i zarządza nimi.</p></li>
+                <li class="def"><span>Kontakt</span><p>przekazanie Użytkownikowi danych kontaktowych do właściciela ogłoszenia.</p></li>
+                <li class="def"><span>Administrator</span><p>Twórca i właściciel serwisu Damin Nagieł</p></li>
+                <li class="def"><span>Regulamin</span><p>Niniejszy regulamin.</p></li>
+            </ol>
+            <div style="clear:both;"></div>
+            
+            <h6>II Ogólne warunki korzystania z serwisu</h6>
+            <ol>
+                <li class="nr"><span>1.</span><p>Regulamin określa zasady korzystania z Lento.pl, prawa i obowiązki Użytkownika, a także prawa, obowiązki i zakres odpowiedzialności Administratora.</p></li>
+                <li class="nr"><span>2.</span><p>Każdy Użytkownik z chwilą akceptacji Regulaminu oświadcza, że zapoznał się z jego treścią i wyraża zgodę na jego warunki.</p></li>
+                <li class="nr"><span>3.</span><p>Administrator zastrzega sobie prawo zmiany Regulaminu w każdej chwili. Nowy regulamin zaczyna obowiązywać z chwilą jego opublikowania w serwisie.</p></li>
+                <li class="nr"><span>4.</span><p>Usługi aktywowane przed wejściem w życie zmiany Regulaminu będą wykonane na dotychczasowych zasadach.</p></li>
+            </ol>
+            
+            <h6>III Ogólne warunki korzystania z serwisu</h6>
+            <ol>
+                <li class="nr"><span>1.</span><p>Lorem ipsum.</p></li>
+                <li class="nr"><span>2.</span><p>Lorem ipsum.</p></li>
+            </ol>
+            
+            <h6>IV Konto użytkownika</h6>
+            <ol>
+                <li class="nr"><span>0.0</span><p>Lorem ipsum.</p></li>
+            </ol>
+            
+            <h6>V Dodawanie i edycja ogłoszeń</h6>
+            <ol>
+                <li class="nr"><span>0.0</span><p>Lorem ipsum.</p></li>
+            </ol>
+            
+            <h6>VI Pozyskiwanie kontaktów</h6>
+            <ol>
+                <li class="nr"><span>0.0</span><p>Lorem ipsum.</p></li>
+            </ol>
+            
+            <h6>VII Odpowiedzialność</h6>
+            <ol>
+                <li class="nr"><span>0.0</span><p>Lorem ipsum.</p></li>
+            </ol>
+            
+            <h6>VIII Reklamacje</h6>
+            <ol>
+                <li class="nr"><span>0.0</span><p>Lorem ipsum.</p></li>
+            </ol>
+            
+            
 	</div>
     </article>
 	
