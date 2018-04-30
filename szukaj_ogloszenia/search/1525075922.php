641aa18a6d70f3b2a9b36e23f2fe12cfbba70873
Damiannagiel <internetowa.gielda.uslug@gmail.com>
2018-04-30 10:14:24 +0200

    0.8.2
    Przerobiono stronę główną na wczytywanie 10 najn owszych ogłoszeń

---
+++ szukaj_ogloszenia/search/1525075922.php
@@ -0,0 +1,607 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<?php
+		include_once "../../szablon/nav_head2.php";
+	?>
+	
+	<title>igu.com.pl - Wyniki wyszukiwania</title>
+	
+	<meta name="description" content="Opis w Google" />
+	<link href="../../kategoria/css/mini.css" type="text/css" rel="stylesheet"/>
+	<link href="../../ogloszenie_wyswietl/mini.css" type="text/css" rel="stylesheet"/>
+	<link href="../wynik.css" type="text/css" rel="stylesheet"/>
+
+</head>
+
+<?php
+	include_once "../../szablon/nav_body2.php";
+	include_once "../../szablon/nav_category2.php";
+?>
+	<header>
+                <h2>Szukaj ogłoszenia</h2>
+		<p class="nawias">(22 ogłoszeń spełniających kryteria)</p>
+	</header>
+	<div class="kat">
+		<div class="filter"><div><span class="type">Tylko ze zdjęciami: </span> <p>TAK</p></div><div><span class="type">Sortuj według: </span><p>Najpopularniejsze</p></div></div>
+		<div class="change">
+			<p><a href="../index.php">zmień kryteria wyszukiwania</a></p>
+		</div>
+	</div>
+	
+	<main><a class="ad" href="../../ogloszenie/Instalacje_Elektryczne_1.php">
+	<article class="ad-mini">
+		<div class="img"><img src="../../ogloszenie/img/1/1.jpg"/></div>		
+			<div class="info">
+				<div class="ogl_mini_txt">
+				
+						<header>
+							<h5>Instalacje Elektryczne</h5>
+						</header>
+						
+					<div class="ogl_mini_opis">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris semper urna vitae est auctor molestie. Ut accumsan viverra ligula id tincidunt. Aliquam mattis!!</p>
+					</div>
+						
+				</div>
+					
+				<div class="ogl_mini_info">
+					<div class="p3"><p>Oferta ważna do:2018-06-30 23:59:59</p></div>
+					<div class="p2"><p>30 zł za punkt</p></div>
+					<div class="p11"><p class="zasieg"><p>pow. radomszczański (łódzkie)</p></p></div>
+					<div class="p4"><p>wyświetleń: 115 kontktów: 4</p></div>
+				</div>
+			</div>
+		<div style="clear:both">
+			
+	</article>
+</a><a class="ad" href="../../ogloszenie/Blacharka_30.php">
+	<article class="ad-mini">
+		<div class="img"><img src="../../ogloszenie/img/30/1.jpg"/></div>		
+			<div class="info">
+				<div class="ogl_mini_txt">
+				
+						<header>
+							<h5>Blacharka</h5>
+						</header>
+						
+					<div class="ogl_mini_opis">
+						<p>Usługi blacharskie na najwyższym poziomie</p>
+					</div>
+						
+				</div>
+					
+				<div class="ogl_mini_info">
+					<div class="p3"><p>Oferta ważna do:2018-06-30 23:59:59</p></div>
+					<div class="p2"><p>300 zł za element</p></div>
+					<div class="p11"><p class="zasieg"><p>Bogatynia<br/>(pow. zgorzelecki woj. dolnośląskie)</p></p></div>
+					<div class="p4"><p>wyświetleń: 86 kontktów: 0</p></div>
+				</div>
+			</div>
+		<div style="clear:both">
+			
+	</article>
+</a><a class="ad" href="../../ogloszenie/Zlece_wykonanie_remontu_lazienki_w_bloku_2_pietro._35.php">
+	<article class="ad-mini">
+		<div class="img"><img src="../../ogloszenie/img/35/1.jpg"/></div>		
+			<div class="info">
+				<div class="ogl_mini_txt">
+				
+						<header>
+							<h5>Zlecę wykonanie remontu łazienki w bloku 2 piętro.</h5>
+						</header>
+						
+					<div class="ogl_mini_opis">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris semper urna vitae est auctor molestie. Ut accumsan viverra ligula id tincidunt. Aliquam mattis!!</p>
+					</div>
+						
+				</div>
+					
+				<div class="ogl_mini_info">
+					<div class="p3"><p>Oferta ważna do:2018-06-30 23:59:59</p></div>
+					<div class="p2"><p>4000 zł do uzgodnienia</p></div>
+					<div class="p11"><p class="zasieg"><p>Wrocław<br/>(woj. dolnośląskie)</p></p></div>
+					<div class="p4"><p>wyświetleń: 81 kontktów: 1</p></div>
+				</div>
+			</div>
+		<div style="clear:both">
+			
+	</article>
+</a><a class="ad" href="../../ogloszenie/Uslugi_Hydrauliczne_3.php">
+	<article class="ad-mini">
+		<div class="img"><img src="../../ogloszenie/img/3/1.jpg"/></div>		
+			<div class="info">
+				<div class="ogl_mini_txt">
+				
+						<header>
+							<h5>Usługi Hydrauliczne</h5>
+						</header>
+						
+					<div class="ogl_mini_opis">
+						<p>Usługi hydrauliczne w najniższych cenach!</p>
+					</div>
+						
+				</div>
+					
+				<div class="ogl_mini_info">
+					<div class="p3"><p>Oferta ważna do:2018-06-30 23:59:59</p></div>
+					<div class="p2"><p>110 zł za punkt</p></div>
+					<div class="p11"><p class="zasieg"><p>łódzkie (4 powiatów)</p> <p>śląskie (częstochowski, kłobucki)</p></p></div>
+					<div class="p4"><p>wyświetleń: 67 kontktów: 2</p></div>
+				</div>
+			</div>
+		<div style="clear:both">
+			
+	</article>
+</a><a class="ad" href="../../ogloszenie/Koszenie_trawnika_36.php">
+	<article class="ad-mini">
+		<div class="img"><img src="../../ogloszenie/img/36/1.jpg"/></div>		
+			<div class="info">
+				<div class="ogl_mini_txt">
+				
+						<header>
+							<h5>Koszenie trawnika</h5>
+						</header>
+						
+					<div class="ogl_mini_opis">
+						<p>Koszenie trawnika raz w tygodniu</p>
+					</div>
+						
+				</div>
+					
+				<div class="ogl_mini_info">
+					<div class="p3"><p>Oferta ważna do:2018-06-30 23:59:59</p></div>
+					<div class="p2"><p>50 zł za usługę</p></div>
+					<div class="p11"><p class="zasieg"><p>Toruń<br/>(woj. kujawsko-pomorskie)</p></p></div>
+					<div class="p4"><p>wyświetleń: 60 kontktów: 0</p></div>
+				</div>
+			</div>
+		<div style="clear:both">
+			
+	</article>
+</a><a class="ad" href="../../ogloszenie/do_edycji_i_usuniecia_39.php">
+	<article class="ad-mini">
+		<div class="img"><img src="../../ogloszenie/img/39/1.jpg"/></div>		
+			<div class="info">
+				<div class="ogl_mini_txt">
+				
+						<header>
+							<h5>do edycji i usunięcia</h5>
+						</header>
+						
+					<div class="ogl_mini_opis">
+						<p>lorem iipsum</p>
+					</div>
+						
+				</div>
+					
+				<div class="ogl_mini_info">
+					<div class="p3"><p>Oferta ważna do:2018-06-30 23:59:59</p></div>
+					<div class="p2"><p>1000 zł za usługę</p></div>
+					<div class="p11"><p class="zasieg"><p>Toruń<br/>(woj. kujawsko-pomorskie)</p></p></div>
+					<div class="p4"><p>wyświetleń: 55 kontktów: 1</p></div>
+				</div>
+			</div>
+		<div style="clear:both">
+			
+	</article>
+</a><a class="ad" href="../../ogloszenie/Najlepsze_suknie_slubne_5.php">
+	<article class="ad-mini">
+		<div class="img"><img src="../../ogloszenie/img/5/1.jpg"/></div>		
+			<div class="info">
+				<div class="ogl_mini_txt">
+				
+						<header>
+							<h5>Najlepsze suknie ślubne</h5>
+						</header>
+						
+					<div class="ogl_mini_opis">
+						<p>Suknie ślubne na wymiar, najlepsza jakość w rozsądnej cenie</p>
+					</div>
+						
+				</div>
+					
+				<div class="ogl_mini_info">
+					<div class="p3"><p>Oferta ważna do:2018-06-30 23:59:59</p></div>
+					<div class="p2"><p>2500 zł za sztukę</p></div>
+					<div class="p11"><p class="zasieg"><p>dolnośląskie (Wrocław, Jelenia Góra)</p> kujawsko-pomorskie (Bydgoszcz)</p></div>
+					<div class="p4"><p>wyświetleń: 43 kontktów: 2</p></div>
+				</div>
+			</div>
+		<div style="clear:both">
+			
+	</article>
+</a><a class="ad" href="../../ogloszenie/Informatyk_31.php">
+	<article class="ad-mini">
+		<div class="img"><img src="../../ogloszenie/img/31/1.jpg"/></div>		
+			<div class="info">
+				<div class="ogl_mini_txt">
+				
+						<header>
+							<h5>Informatyk</h5>
+						</header>
+						
+					<div class="ogl_mini_opis">
+						<p>Szukam kogoś kto złoży mi PC</p>
+					</div>
+						
+				</div>
+					
+				<div class="ogl_mini_info">
+					<div class="p3"><p>Oferta ważna do:2018-06-30 23:59:59</p></div>
+					<div class="p2"><p>do uzgodnienia</p></div>
+					<div class="p11"><p class="zasieg"><p>kujawsko-pomorskie (całe)</p></p></div>
+					<div class="p4"><p>wyświetleń: 38 kontktów: 2</p></div>
+				</div>
+			</div>
+		<div style="clear:both">
+			
+	</article>
+</a><a class="ad" href="../../ogloszenie/szukam_sprzataczki_41.php">
+	<article class="ad-mini">
+		<div class="img"><img src="../../ogloszenie/img/41/1.jpg"/></div>		
+			<div class="info">
+				<div class="ogl_mini_txt">
+				
+						<header>
+							<h5>szukam sprzątaczki</h5>
+						</header>
+						
+					<div class="ogl_mini_opis">
+						<p>Szukam pani do sprzątania domu jednorodzinnego (ok 150m2) dwa razy w tygodniu, wtorki i piątki, pora do ustalenia</p>
+					</div>
+						
+				</div>
+					
+				<div class="ogl_mini_info">
+					<div class="p3"><p>Oferta ważna do:2018-06-30 23:59:59</p></div>
+					<div class="p2"><p>15 zł za godzinę</p></div>
+					<div class="p11"><p class="zasieg"><p>Lublin<br/>(woj. lubelskie)</p></p></div>
+					<div class="p4"><p>wyświetleń: 38 kontktów: 0</p></div>
+				</div>
+			</div>
+		<div style="clear:both">
+			
+	</article>
+</a><a class="ad" href="../../ogloszenie/Korepetycje_z_Angielskiego_7.php">
+	<article class="ad-mini">
+		<div class="img"><img src="../../ogloszenie/img/7/1.jpg"/></div>		
+			<div class="info">
+				<div class="ogl_mini_txt">
+				
+						<header>
+							<h5>Korepetycje z Angielskiego</h5>
+						</header>
+						
+					<div class="ogl_mini_opis">
+						<p>Lorem Ipsum</p>
+					</div>
+						
+				</div>
+					
+				<div class="ogl_mini_info">
+					<div class="p3"><p>Oferta ważna do:2018-06-30 23:59:59</p></div>
+					<div class="p2"><p>40 zł za godzinę</p></div>
+					<div class="p11"><p class="zasieg"><p>pow. kluczborski (opolskie)</p></p></div>
+					<div class="p4"><p>wyświetleń: 36 kontktów: 0</p></div>
+				</div>
+			</div>
+		<div style="clear:both">
+			
+	</article>
+</a><a class="ad" href="../../ogloszenie/Szukam_korepetytora_jezyka_Polskiego_38.php">
+	<article class="ad-mini">
+		<div class="img"><img src="../../ogloszenie/img/38/1.jpg"/></div>		
+			<div class="info">
+				<div class="ogl_mini_txt">
+				
+						<header>
+							<h5>Szukam korepetytora języka Polskiego</h5>
+						</header>
+						
+					<div class="ogl_mini_opis">
+						<p>Szukam korepetytora bądź korepetytorki języka Polskiego. Lekcje w moim mieszkaniu w centrum Wrocławia 3 razy w tygodniu.</p>
+					</div>
+						
+				</div>
+					
+				<div class="ogl_mini_info">
+					<div class="p3"><p>Oferta ważna do:2018-06-30 23:59:59</p></div>
+					<div class="p2"><p>50 zł za godzinę</p></div>
+					<div class="p11"><p class="zasieg"><p>Wrocław<br/>(woj. dolnośląskie)</p></p></div>
+					<div class="p4"><p>wyświetleń: 34 kontktów: 0</p></div>
+				</div>
+			</div>
+		<div style="clear:both">
+			
+	</article>
+</a><a class="ad" href="../../ogloszenie/Wykonanie_i_montaz_bramy_kutej_34.php">
+	<article class="ad-mini">
+		<div class="img"><img src="../../ogloszenie/img/34/1.jpg"/></div>		
+			<div class="info">
+				<div class="ogl_mini_txt">
+				
+						<header>
+							<h5>Wykonanie i montaż bramy kutej</h5>
+						</header>
+						
+					<div class="ogl_mini_opis">
+						<p>Zlecę wykonanie i montaż bramy kutej w miejscowości puławy, najlepiej firmi</p>
+					</div>
+						
+				</div>
+					
+				<div class="ogl_mini_info">
+					<div class="p3"><p>Oferta ważna do:2018-06-30 23:59:59</p></div>
+					<div class="p2"><p>do uzgodnienia</p></div>
+					<div class="p11"><p class="zasieg"><p>Kłobuck<br/>(pow. kłobucki woj. śląskie)</p></p></div>
+					<div class="p4"><p>wyświetleń: 30 kontktów: 1</p></div>
+				</div>
+			</div>
+		<div style="clear:both">
+			
+	</article>
+</a><a class="ad" href="../../ogloszenie/Test_5_51.php">
+	<article class="ad-mini">
+		<div class="img"><img src="../../ogloszenie/img/51/1.jpg"/></div>		
+			<div class="info">
+				<div class="ogl_mini_txt">
+				
+						<header>
+							<h5>Test 5</h5>
+						</header>
+						
+					<div class="ogl_mini_opis">
+						<p>sdfsd fds f</p>
+					</div>
+						
+				</div>
+					
+				<div class="ogl_mini_info">
+					<div class="p3"><p>Oferta ważna do:2018-06-30 23:59:59</p></div>
+					<div class="p2"><p>do uzgodnienia</p></div>
+					<div class="p11"><p class="zasieg"><p>lubuskie (całe)</p></p></div>
+					<div class="p4"><p>wyświetleń: 24 kontktów: 0</p></div>
+				</div>
+			</div>
+		<div style="clear:both">
+			
+	</article>
+</a><a class="ad" href="../../ogloszenie/KAwa2_52.php">
+	<article class="ad-mini">
+		<div class="img"><img src="../../ogloszenie/img/52/1.jpg"/></div>		
+			<div class="info">
+				<div class="ogl_mini_txt">
+				
+						<header>
+							<h5>KAwa2</h5>
+						</header>
+						
+					<div class="ogl_mini_opis">
+						<p>fd df df dfd </p>
+					</div>
+						
+				</div>
+					
+				<div class="ogl_mini_info">
+					<div class="p3"><p>Oferta ważna do:2018-06-30 23:59:59</p></div>
+					<div class="p2"><p>150 zł za punkt</p></div>
+					<div class="p11"><p class="zasieg"><p>kujawsko-pomorskie (całe)</p></p></div>
+					<div class="p4"><p>wyświetleń: 21 kontktów: 0</p></div>
+				</div>
+			</div>
+		<div style="clear:both">
+			
+	</article>
+</a><a class="ad" href="../../ogloszenie/miniaturka_test_2_57.php">
+	<article class="ad-mini">
+		<div class="img"><img src="../../ogloszenie/img/57/1.jpg"/></div>		
+			<div class="info">
+				<div class="ogl_mini_txt">
+				
+						<header>
+							<h5>miniaturka test 2</h5>
+						</header>
+						
+					<div class="ogl_mini_opis">
+						<p>Ratujmy żubry w puszczy białowieskiej</p>
+					</div>
+						
+				</div>
+					
+				<div class="ogl_mini_info">
+					<div class="p3"><p>Oferta ważna do:2018-06-30 23:59:59</p></div>
+					<div class="p2"><p>100 zł za godzinę</p></div>
+					<div class="p11"><p class="zasieg"><p>Białowieża<br/>(pow. hajnowski woj. podlaskie)</p></p></div>
+					<div class="p4"><p>wyświetleń: 17 kontktów: 0</p></div>
+				</div>
+			</div>
+		<div style="clear:both">
+			
+	</article>
+</a><a class="ad" href="../../ogloszenie/Geocaching_47.php">
+	<article class="ad-mini">
+		<div class="img"><img src="../../ogloszenie/img/47/1.jpg"/></div>		
+			<div class="info">
+				<div class="ogl_mini_txt">
+				
+						<header>
+							<h5>Geocaching</h5>
+						</header>
+						
+					<div class="ogl_mini_opis">
+						<p>opis ogłoszenia</p>
+					</div>
+						
+				</div>
+					
+				<div class="ogl_mini_info">
+					<div class="p3"><p>Oferta ważna do:2018-06-30 23:59:59</p></div>
+					<div class="p2"><p>25 zł za godzinę</p></div>
+					<div class="p11"><p class="zasieg"><p>Łódź<br/>(woj. łódzkie)</p></p></div>
+					<div class="p4"><p>wyświetleń: 12 kontktów: 0</p></div>
+				</div>
+			</div>
+		<div style="clear:both">
+			
+	</article>
+</a><a class="ad" href="../../ogloszenie/miniaturka_56.php">
+	<article class="ad-mini">
+		<div class="img"><img src="../../ogloszenie/img/56/1.jpg"/></div>		
+			<div class="info">
+				<div class="ogl_mini_txt">
+				
+						<header>
+							<h5>miniaturka</h5>
+						</header>
+						
+					<div class="ogl_mini_opis">
+						<p>dasdas sad sad asd as a</p>
+					</div>
+						
+				</div>
+					
+				<div class="ogl_mini_info">
+					<div class="p3"><p>Oferta ważna do:2018-06-30 23:59:59</p></div>
+					<div class="p2"><p>10 zł za godzinę</p></div>
+					<div class="p11"><p class="zasieg"><p>Warszawa<br/>(woj. mazowieckie)</p></p></div>
+					<div class="p4"><p>wyświetleń: 12 kontktów: 0</p></div>
+				</div>
+			</div>
+		<div style="clear:both">
+			
+	</article>
+</a><a class="ad" href="../../ogloszenie/To_jest_najdluzszy_mozliwy_tytul_ogloszenia_na_igu_32.php">
+	<article class="ad-mini">
+		<div class="img"><img src="../../ogloszenie/img/32/1.jpg"/></div>		
+			<div class="info">
+				<div class="ogl_mini_txt">
+				
+						<header>
+							<h5>To jest najdłuższy możliwy tytuł ogłoszenia na igu</h5>
+						</header>
+						
+					<div class="ogl_mini_opis">
+						<p>Lorem Ipsum</p>
+					</div>
+						
+				</div>
+					
+				<div class="ogl_mini_info">
+					<div class="p3"><p>Oferta ważna do:2018-06-30 23:59:59</p></div>
+					<div class="p2"><p>do uzgodnienia</p></div>
+					<div class="p11"><p class="zasieg"><p>Nowa Brzeźnica<br/>(pow. radomszczański woj. łódzkie)</p></p></div>
+					<div class="p4"><p>wyświetleń: 10 kontktów: 1</p></div>
+				</div>
+			</div>
+		<div style="clear:both">
+			
+	</article>
+</a><a class="ad" href="../../ogloszenie/przykladowe_ogloszenie_5_29.php">
+	<article class="ad-mini">
+		<div class="img"><img src="../../ogloszenie/img/29/1.jpg"/></div>		
+			<div class="info">
+				<div class="ogl_mini_txt">
+				
+						<header>
+							<h5>przykładowe ogłoszenie 5</h5>
+						</header>
+						
+					<div class="ogl_mini_opis">
+						<p>dobry opis</p>
+					</div>
+						
+				</div>
+					
+				<div class="ogl_mini_info">
+					<div class="p3"><p>Oferta ważna do:2018-06-30 23:59:59</p></div>
+					<div class="p2"><p>do uzgodnienia</p></div>
+					<div class="p11"><p class="zasieg"><p>Zamość<br/>(woj. lubelskie)</p></p></div>
+					<div class="p4"><p>wyświetleń: 9 kontktów: 1</p></div>
+				</div>
+			</div>
+		<div style="clear:both">
+			
+	</article>
+</a><a class="ad" href="../../ogloszenie/uslugi_kosmetyczne_33.php">
+	<article class="ad-mini">
+		<div class="img"><img src="../../ogloszenie/img/33/1.jpg"/></div>		
+			<div class="info">
+				<div class="ogl_mini_txt">
+				
+						<header>
+							<h5>usługi kosmetyczne</h5>
+						</header>
+						
+					<div class="ogl_mini_opis">
+						<p>Paznokcie</p>
+					</div>
+						
+				</div>
+					
+				<div class="ogl_mini_info">
+					<div class="p3"><p>Oferta ważna do:2018-06-30 23:59:59</p></div>
+					<div class="p2"><p>30 zł za usługę</p></div>
+					<div class="p11"><p class="zasieg"><p>Pajęczno<br/>(pow. pajęczański woj. łódzkie)</p></p></div>
+					<div class="p4"><p>wyświetleń: 7 kontktów: 0</p></div>
+				</div>
+			</div>
+		<div style="clear:both">
+			
+	</article>
+</a><a class="ad" href="../../ogloszenie/Test_6_50.php">
+	<article class="ad-mini">
+		<div class="img"><img src="../../ogloszenie/img/50/1.jpg"/></div>		
+			<div class="info">
+				<div class="ogl_mini_txt">
+				
+						<header>
+							<h5>Test 6</h5>
+						</header>
+						
+					<div class="ogl_mini_opis">
+						<p>dsfdsf sdf sdfsd </p>
+					</div>
+						
+				</div>
+					
+				<div class="ogl_mini_info">
+					<div class="p3"><p>Oferta ważna do:2018-06-30 23:59:59</p></div>
+					<div class="p2"><p>do uzgodnienia</p></div>
+					<div class="p11"><p class="zasieg"><p>Krosno<br/>(woj. podkarpackie)</p></p></div>
+					<div class="p4"><p>wyświetleń: 4 kontktów: 0</p></div>
+				</div>
+			</div>
+		<div style="clear:both">
+			
+	</article>
+</a><a class="ad" href="../../ogloszenie/przyklad_do_usuniecia_46.php">
+	<article class="ad-mini">
+		<div class="img"><img src="../../ogloszenie/img/46/1.jpg"/></div>		
+			<div class="info">
+				<div class="ogl_mini_txt">
+				
+						<header>
+							<h5>przykład do usunięcia</h5>
+						</header>
+						
+					<div class="ogl_mini_opis">
+						<p>asdsad d asdsa </p>
+					</div>
+						
+				</div>
+					
+				<div class="ogl_mini_info">
+					<div class="p3"><p>Oferta ważna do:2018-06-30 23:59:59</p></div>
+					<div class="p2"><p>150 zł za m3</p></div>
+					<div class="p11"><p class="zasieg"><p>Poznań<br/>(woj. wielkopolskie)</p></p></div>
+					<div class="p4"><p>wyświetleń: 0 kontktów: 0</p></div>
+				</div>
+			</div>
+		<div style="clear:both">
+			
+	</article>
+</a></main>
+<?php
+	include_once "../../szablon/stopka2.php";
+?>
