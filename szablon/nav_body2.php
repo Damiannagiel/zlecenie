2c06c8c09b01bf71dfdb4761c6fa2792d040a562
Damiannagiel <internetowa.gielda.uslug@gmail.com>
2018-06-01 17:05:36 +0200

    0.8.15

--- szablon/nav_body2.php
+++ szablon/nav_body2.php
@@ -2,7 +2,7 @@
 		<div id="spacer1"></div>
 		<header class="site-header">
                     <div class="max_width">
-			<a class="logo" href="http://localhost/zlecenie/index.php" target="_blank">igu.com.pl</a>
+			<a class="logo" href="http://localhost/zlecenie/index.php" target="_blank">tuUslugi.pl</a>
 			<button class="hamburger" type="button" title="Otwórz menu">
 							<svg height="32px" id="Layer_1" style="enable-background:new 0 0 32 32;" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z"/></svg>
 							<svg enable-background="new 0 0 32 32" height="32px" id="svg2" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:svg="http://www.w3.org/2000/svg"><g id="background"><rect fill="none" height="32" width="32"/></g><g id="cancel"><polygon points="2,26 6,30 16,20 26,30 30,26 20,16 30,6 26,2 16,12 6,2 2,6 12,16  "/></g></svg>
